30457c090848017ca7e00e7cf5aba320354a349a
roman <kolbasilyvasily@yandex.ru>
2026-05-09 00:41:29 +0500

    Заменил свойства на вызов методов интерфейса IBMPCommon.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # This is a sample Python script.
-import time
 import bmp581mod
+from time import sleep_ms
 from machine import I2C, Pin
 from micropython import const
 from sensor_pack_2.bus_service import I2cAdapter
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     ps = bmp581mod.Bmp581(adapter=adapter, address=SENSOR_ADDR)
     # программный сброс датчика
     ps.soft_reset()
-    time.sleep_ms(2)    # продолжительность программного сброса не более 2 мс!
+    sleep_ms(2)    # продолжительность программного сброса не более 2 мс!
     #
     ps.init_hardware()
     # если у вас посыпались исключения, то проверьте все соединения!
@@ -55,34 +55,35 @@ if __name__ == '__main__':
 
     print(txt_break)
     print("Текущие настройки, считанные из датчика.")
-    _oversampling = ps.oversampling
-    _pwr_mode = ps.power_mode
-    _output_data_rate = ps.output_data_rate
-    iir_conf = ps.iir_config
+    _oversampling = ps.set_oversampling(None, None)
+    _pwr_mode = ps.set_power_mode(None)
+    _output_data_rate = ps.set_sampling_period(None)
+    iir_conf = ps.set_iir_filter(None, None)         # (temp_iir, press_iir)
 
     print(f"power mode: {_pwr_mode}; output data rate: {_output_data_rate}")
     print(f"pressure oversampling: {_oversampling[0]}; temperature oversampling: {_oversampling[1]}")
-    print(f"pressure iir: {iir_conf[0]}; temperature iir: {iir_conf[1]}")
+    print(f"pressure iir: {iir_conf[1]}; temperature iir: {iir_conf[0]}")
     print(txt_break)
 
-    osr_r = ps.effective_osr_rating
+    osr_r = ps.get_effective_osr_rating()
     print(f"OSR Tэфф: {osr_r[0]}; OSR Pэфф: {osr_r[1]}; Настройка ODR верна: {osr_r[2]}")
 
     print(txt_break)
     print("Измеряется только температура!")
-    ps.temperature_only = True
-    ps.temp_oversampling = 4
-    ps.pressure_oversampling = 4
-    odr = 10    # частота выдачи данных в Гц
+    ps.set_channels(temp_en=True, press_en=False)  # Только температура
+    ps.set_oversampling(temp=4, press=4)  # press игнорируется при press_en=False
+    odr = 10    # индекс(!) частоты выдачи данных
     conv_time = ps.get_conversion_cycle_time()  # возвращает мс
     wt_ms = get_wait_time_ms(odr, conv_time)
     print(f"Период выдачи данных [мс]: {wt_ms}")
-    tmp = ps.start_measurement(mode=1, output_data_rate=odr)
+    ps.set_power_mode(value=1)  # Normal
+    ps.set_sampling_period(odr)
+    tmp = ps.start_measurement()
     if tmp:
         print("Частота обновления данных датчиком соответствует значениям передискретизации температуры и давления!")
 
     for _ in range(ITERATIONS):
-        time.sleep_ms(wt_ms)
+        sleep_ms(wt_ms)
         if ps.is_data_ready():
             temperature = ps.get_temperature()
             print(f"Температура [гр. Ц.]: {temperature}")
@@ -90,13 +91,15 @@ if __name__ == '__main__':
             print(f"Нет данных для считывания!")
     print(txt_break)
     print("Измеряется температура и давление!")
-    ps.temperature_only = False
-    odr = 20
+    ps.set_channels(temp_en=True, press_en=True)  # Температура + давление
+    odr = 20    # индекс(!) частоты выдачи данных
     conv_time = ps.get_conversion_cycle_time()  # возвращает мс
     wt_ms = get_wait_time_ms(odr, conv_time)
-    tmp = ps.start_measurement(mode=1, output_data_rate=odr)
-    for _ in range(200):
-        time.sleep_ms(wt_ms)
+    ps.set_power_mode(value=1)
+    ps.set_sampling_period(odr)
+    tmp = ps.start_measurement()
+    for _ in range(ITERATIONS):
+        sleep_ms(wt_ms)
         if ps.is_data_ready():
             temperature, pressure = ps.get_temperature(), ps.get_pressure()
             print(f"Температура [гр. Ц.]: {temperature}; Давление [Па]: {pressure}")
@@ -104,7 +107,7 @@ if __name__ == '__main__':
             print(f"Нет данных для считывания!")
 
     print(txt_break)
-    odr = 5     # частота получения данных в режиме измерений по запросу
+    odr = 5     # индекс(!) частоты выдачи данных
     conv_time = ps.get_conversion_cycle_time()  # возвращает мс
     wt_ms = get_wait_time_ms(odr, conv_time)
     print("Режим измерения по запросу! Температура и давление!")
@@ -112,14 +115,20 @@ if __name__ == '__main__':
     # Режим измерения по запросу рекомендуется для приложений, которым требуется очень низкая частота дискретизации или
     # синхронизация на базе хоста. Режим измерения по запросу также можно использовать, если требуется ODR выше 240 Гц.
     # задержка перед первым next(), чтобы датчик успел подготовить данные
-    time.sleep_ms(wt_ms)
-    for items in ps:
-        if items:
-            pressure, temperature = items
-            print(f"Температура [гр. Ц.]: {temperature}; Давление [Па]: {pressure}")
-        else:
-            print(f"Нет данных для считывания!")
-        tmp = ps.start_measurement(mode=2, output_data_rate=0)
-        time.sleep_ms(wt_ms)
+    ps.temperature_only = False
+    ps.set_power_mode(value=2)  # Forced
+    ps.set_sampling_period(0)  # ODR игнорируется в Forced mode
 
+    for _ in range(ITERATIONS):
+        ps.start_measurement()  # Запускаю измерение
+        min_conv = ps.get_conversion_cycle_time()
+        sleep_ms(max(wt_ms, int(min_conv) + 2))    # Жду
+
+        if ps.is_data_ready():
+            # Читаю напрямую, без итератора!
+            t = ps.get_temperature()
+            p = ps.get_pressure()
+            print(f"Температура [гр. Ц.]: {t}; Давление [Па]: {p}")
+        else:
+            print("Данные не готовы!")
     print(txt_break)
